7fd8f92e2ce3a3203779e251e82f01dabf9b20e0
Alex Craig <acraig780@gmail.com>
2021-08-02 11:12:44 -0400

    example api usage

--- src/cardLinker.js
+++ src/cardLinker.js
@@ -7,11 +7,21 @@ export class CardLinker extends LitElement {
 
   connectedCallback() {
     super.connectedCallback();
+
+    this.fetchCard();
+  }
+
+  fetchCard() {
+    fetch(`https://api.scryfall.com/cards/named?fuzzy=${this.textContent}&format=image&version=medium`)
+      .then(resp => {
+        this.imageUrl = resp.url;
+        this.requestUpdate();
+      });
   }
 
   render() {
     return html`
-      <div>Hello world</div>
+      <a href=${this.imageUrl} target='_blank' rel='nofollow noreferrer noopener'><slot></slot></a>
     `;
   }
 }
